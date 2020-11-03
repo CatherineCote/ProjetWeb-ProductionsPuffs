7b28bb6243ab4c6710f9eba58d2ce59715984549
CatherineCote <54541762+CatherineCote@users.noreply.github.com>
2020-11-03 17:00:23 -0500

    Page_Accueil_HTML/CSS[suite]

--- wp-content/page-accueil.php
+++ wp-content/page-accueil.php
@@ -37,7 +37,7 @@ h2 {
 	text-align: center;
 	width:30%;
 	letter-spacing: 8px;
-	font-size:20px;
+	font-size:100%;
 	line-height: 3;
 	transform:translateX(50%);
 }
@@ -61,6 +61,8 @@ h2 {
 	height:15vmin;
 	margin-right:5%;
 }
+
+
 .testament{
 	border:none;
 	background-color:rgba(9, 118, 165,0.5);
@@ -72,10 +74,10 @@ h2 {
 .raison{
 	color:white;
 	width:40%;
+	height:20vmin;
 	letter-spacing: 3px;
 	font-size:14px;
 	background-color:rgba(9, 118, 165,0.5);
-	padding:5px;
 	margin-right:19%;
 	margin-top:10%;
 	display:flex;
@@ -84,40 +86,116 @@ h2 {
 	text-align:center;
 	transform:translateX(10%);
 }
+.wrapperRaison{
+	display:flex;
+	width:100%;
+	justify-content:flex-end;
+}
+.temoin{
+	text-align:center;
+	color:white;
+	width:40%;
+	height:20vmin;
+	font-size:14px;
+	line-height: 3;
+	background-color:rgba(0, 0, 0,0.5);
+	transform:translateY(-25%);
+}
+#nom{
+	font-size:12px;
+}
+.carrousel{
+	text-align:center;
+	color:white;
+	width:60%;
+	height:30vmin;
+	font-size:12px;
+	line-height: 5;
+	background-color:rgba(0, 0, 0,0.5);
+	margin-left:18%;
+}
+.wrapperCarrousel{
+position:absolute;
+display:flex;
+justify-content: space-evenly;
+align-items:flex-end;
+width:80%;
+height:80vmin;
+right:15%;
+z-index:4;
+}
+.wrapperService{
+	display:flex;
+	width:40%;
+	height:100%;
+	align-self:flex-end;
+	flex-wrap:wrap;
+	transform:translateY(100%) translateX(50%);
+	position:relative;
+	
+}
+.hebergement{
+	display:flex;
+	align-items:center;
+	justify-content:center;
+	width:100%;
+	height:15vmin;
+	margin-top:15%;
+	background-color:rgba(0, 0, 0,0.5);
+	color:white;
+	position:relative;
+}
+.ecoute{
+	display:flex;
+	align-items:center;
+	justify-content:center;
+	width:100%;
+	height:15vmin;
+	background-color:rgba(0, 0, 0,0.5);
+	color:white;
+	position:relative;
+}
 
 .blockBleu{
 	width:100%;
-	height:100vmin;
+	height:1100px;
 	clip-path: polygon(0 0, 100% 41%, 100% 100%, 0% 100%);
 	background-color:rgba(9, 118, 165,0.5);
 	
 }
 .joindre{
+	position:absolute;
 	display:flex;
+	width:100%;
+}
+.sectionFlex{
+ display:flex;
 	justify-content:center;
 	align-items:center;
+	flex-wrap:nowrap;
 	width:40%;
 	height:50vmin;
 	color:white;
 	background: rgba(0, 0, 0, 0.5);
 	position:absolute;
 	right:15%;
-	transform:translateY(110%);
+	transform:translateY(100%);
+	margin-top:20%;
 	z-index:1;
-	
 }
-.joindre:before{
+
+.joindre:after{
 	display:none;
  content: '';
  background: white;
- width:110%; 
+ width:100vmin; 
  position: absolute;
  left: 0;
  right: 0;
  top: 0; 
  bottom: 0;
  filter: blur(4px);
- z-index: 6;
+ z-index:-1;
   opacity: 0.5;
 }
 .joindre p{
@@ -130,6 +208,37 @@ h2 {
 	letter-spacing: 2px;
 	font-size:10px;
 	line-height: 2;
+}
+.wrapperRappel{
+	display:flex;
+	height:100%;
+	flex-wrap:nowrap;
+	align-self:flex-end;
+	transform:translateY(200%) translateX(-50%);
+	position:relative;
+}
+.don02{
+	display:flex;
+	align-items:center;
+	justify-content:center;
+	width:100%;
+	border:none;
+	height:10vmin;
+	background-color:rgba(0, 0, 0,0.5);
+	color:white;
+	position:relative;
+}
+.benevole{
+	display:flex;
+	align-items:center;
+	justify-content:center;
+	width:100%;
+	border:none;
+	margin-left:10%;
+	height:10vmin;
+	background-color:rgba(0, 0, 0,0.5);
+	color:white;
+	position:relative;
 }
 
 </style>
@@ -155,11 +264,22 @@ h2 {
 	<button type="button" class="don"> Faire un don</button>
 	<button type="button" class="testament"> Don par testament</button>
 	</div>
-	<div class="raison">
+	<div class="wrapperRaison"><div class="raison">
 		<h4>Pourquoi faire un don</h4>
 		<p>Les dons nous permettent d'aider les femmes en détresses.</p>
-	</div>
-	<div class="blur">
+	</div></div>
+
+	<div class="wrapperCarrousel">
+		<div class="temoin">
+			<p>"Cette endroit a changé ma vie"</p>
+			<p id="nom">-Mme Tremblay</p>
+		</div>
+	<div class="carrousel">
+		<p>Nouvelles</p>
+		<p>Évènement</p>
+	</div></div>
+	
+	<div class="sectionFlex">
 		<div class="joindre">
 		<p>JOINDRE
 		LE CHAÎNON<br><br>
@@ -174,9 +294,21 @@ h2 {
 		Montréal (Québec) H2W 1Z8
 		514 843-4354
 		514 845-0151</span></p>
-	</div></div>
-	
+	</div>
+		<div class="wrapperService">
+		<div class="hebergement">Service d'hébergement</div>
+		<div class="ecoute">Service d'écoute</div>
+		</div>
+
+		<div class="wrapperRappel">
+		<button type="button" class="don02"> Faire un don</button>
+		<button type="button" class="benevole"> Devenez bénévole</button>
 		
+		</div>
+	
+	</div>
+		
+	
 		
 		</div>
 		<div class=blockBleu>
