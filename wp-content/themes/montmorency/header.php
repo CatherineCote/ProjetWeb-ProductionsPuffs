c1a3e9f18c2d5fb1afaa3407bbbb67387d3744b0
LuckyM <54539156+NicolasReilly@users.noreply.github.com>
2020-12-01 13:10:54 -0500

    Add files via upload

--- wp-content/themes/montmorency/header.php
+++ wp-content/themes/montmorency/header.php
@@ -382,6 +382,20 @@ a {
 		</div>
 	</div>
 	<script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js'></script><script  src="./script.js"></script>
+	<script>
+		const burger = document.querySelector(".burger");
+		const menu = document.querySelector(".menu");
+
+		burger.addEventListener("click", () => {
+		if (menu.style.display == "none" && menu.style.opacity == "0") {
+			gsap.to(".menu", 0.1, { display: "flex" });
+			gsap.to(".menu", 1, { opacity: 0.8 });
+		} else {
+			gsap.to(".menu", 1, { opacity: 0 });
+			gsap.to(".menu", 1.5, { display: "none" });
+		}
+		});
+	</script>
 	<h1>
 		<a href="<?php echo esc_url( home_url( '/' ) ); // Lien vers la page d'accueil ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); // Title it with the blog name ?>" rel="home"><?php bloginfo( 'name' ); // Affiche le nom du site ?></a>
 	</h1>
